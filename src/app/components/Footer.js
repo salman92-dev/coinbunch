8e69c9d1ae7cf4bf502d0e796b3c650c6787588a
salman92-dev <124353689+salman92-dev@users.noreply.github.com>
2025-08-07 13:48:23 +0500

    Add files via upload

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -3,25 +3,42 @@ const Footer = () => {
 
     return (
         <footer className="bg-white/5 md:px-26 px-12 py-12">
-            <div className="flex justify-between flex-wrap gap-y-8">
+            <div className="flex justify-between flex-wrap gap-y-12">
                 <div className="flex flex-col max-md:items-center gap-6 w-full md:w-[50%]">
                     <Image className="w-40" src="/logo.png" alt="logo" width={338} height={68} priority />
-                    <p className="manrope-regular text-base tracking-[0.5px] text-white">© CoinBuch - All Rights Reserved.</p>
-                </div>
-                <div className="flex flex-col items-end max-md:items-center gap-6 w-full md:w-[50%]">
-                    <p className="manrope-regular text-base tracking-[0.5px] text-white">Community</p>
                     <div className="flex items-center gap-4">
                         <a href="">
-                            <Image src="/fb.svg" width={24} height={24} alt="fb" />
+                            <Image className="w-14" src="/tg.svg" width={24} height={24} alt="fb" />
                         </a>
                         <a href="">
-                            <Image src="/x.svg" width={24} height={24} alt="fb" />
+                            <Image className="w-14" src="/x.svg" width={24} height={24} alt="fb" />
                         </a>
                         <a href="">
-                            <Image src="/tg.svg" width={24} height={24} alt="fb" />
+                            <Image className="w-14" src="/yt.svg" width={24} height={24} alt="fb" />
+                        </a>
+                        <a href="">
+                            <Image className="w-14" src="/medium.svg" width={24} height={24} alt="fb" />
                         </a>
                     </div>
-                    <div className="flex items-center gap-4 text-white">
+                    <p className="manrope-regular text-base tracking-[0.5px] text-white">© CoinBunch - All Rights Reserved.</p>
+                </div>
+                <div className="flex flex-col items-start max-md:items-center gap-6 w-full md:w-[30%]">
+                    <p className="manrope-regular text-base tracking-[0.5px] text-white">Buy Crypto With</p>
+                    <div className="flex items-center gap-4">
+                        <a href="">
+                            <Image className="w-20" src="/visa.svg" width={24} height={24} alt="fb" />
+                        </a>
+                        <a href="">
+                            <Image className="w-20" src="/g-pay.svg" width={24} height={24} alt="fb" />
+                        </a>
+                        <a href="">
+                            <Image className="w-20" src="/a-pay.svg" width={24} height={24} alt="fb" />
+                        </a>
+                        <a href="">
+                            <Image className="w-20" src="/m-card.svg" width={24} height={24} alt="fb" />
+                        </a>
+                    </div>
+                    <div className="flex items-center gap-4 text-white md:ml-auto">
                         <a href="">
                             Privacy Policy
                         </a>
