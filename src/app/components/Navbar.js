3b16fd77e915a35bd8b7a4e635f0e7d56ab2df4b
salman92-dev <124353689+salman92-dev@users.noreply.github.com>
2025-08-07 06:34:51 +0500

    Add files via upload

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -39,10 +39,11 @@ const path = usePathname();
         </div>
       </div>
       <div className="hidden md:flex gap-12 ml-12 manrope-regular text-base">
-          <Link href="/" className={`link-hover ${index === 'home' ? 'active' : ''}`}>Features</Link>
-          <Link href="/secrets"  className={`link-hover ${index === 'explore' ? 'active' : ''}`}>Pricing</Link>
-          <Link href="/submit"  className={`link-hover ${index === 'submit' ? 'active' : ''}`}>Blog</Link>
-          <Link href="/submit"  className={`link-hover ${index === 'submit' ? 'active' : ''}`}>Contact</Link>
+          <Link href="#stats">Statistics</Link>
+          <Link href="#howitworks">How it works</Link>
+          <Link href="#why-choose">Why Choose Us?</Link>
+          <Link href="#reviews">User Reviews</Link>
+          <Link href="#faq">FAQ</Link> 
         </div>
 
       <div className="flex items-center space-x-4">
@@ -80,10 +81,10 @@ const path = usePathname();
         <div className={`md:hidden absolute top-[4rem] left-0 right-0 sulphur-regular bg-[#000] flex flex-col justify-center px-6 gap-12 py-12 h-[100vh] w-[100%] transition-transform duration-300 ease-in-out ${menuOpen ? 'translate-y-0' : '-translate-y-full'}`}>
           <Image className={`absolute top-0 left-0 ${menuOpen ? 'opacity-100' : 'opacity-0 duration-400' }`} src="/navbg.png" width={237} height={852} alt='bg' />
           <Image className={`w-[55%] absolute top-0 right-0 transition delay-500 duration-500 origin-bottom-left ${menuOpen ? 'opacity-100 rotate-0' : 'opacity-0 rotate-15' }`} src="/nav-mobile.png" width={237} height={852} alt='bg' />
-          <Link href="/" className="hover:scale(1.1) text-2xl">Statistics</Link>
-          <Link href="/secrets" className="text-2xl">How it works</Link>
-          <Link href="/submit" className="text-2xl">Why Choose Us?</Link>
-          <Link href="/submit" className="text-2xl">User Reviews</Link>
+          <Link href="#stats" className="hover:scale(1.1) text-2xl">Statistics</Link>
+          <Link href="#howitworks" className="text-2xl">How it works</Link>
+          <Link href="#why-choose" className="text-2xl">Why Choose Us?</Link>
+          <Link href="#reviews" className="text-2xl">User Reviews</Link>
           <Link href="#faq" className="text-2xl">FAQ</Link>
 
         </div>
